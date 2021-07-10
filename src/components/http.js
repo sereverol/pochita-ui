0b23a5c96d59fbf4b25fc73ec3ab0a0d19e0c089
juan socorro <46764865+juansm17@users.noreply.github.com>
2021-07-10 17:05:05 -0400

    Update http.js

--- src/components/http.js
+++ src/components/http.js
@@ -7,8 +7,8 @@ const send = async (method, endpoint, body) => {
     let response;
 
     (body == null) 
-    ? response = await fetch(url + endpoint , { method, mode: 'cors', headers })
-    : response = await fetch(url + endpoint , { method, mode: 'cors', headers, body: JSON.stringify(body) });
+    ? response = await fetch(urlx + endpoint , { method, mode: 'cors', headers })
+    : response = await fetch(urlx + endpoint , { method, mode: 'cors', headers, body: JSON.stringify(body) });
     
     return await response.json(); 
 }
